9e42b999e592cb95011db0ae60d52ba1abfe4ac3
Alec Larson <alec.stanford.larson@gmail.com>
2018-12-21 16:29:19 -0500

    tweak the debug logs (#93)
    * log: resolve package.json of core plugins
    
    By resolving the "package.json" of core plugins, we can be sure the resolved path is the plugin`s root directory. This lets us use `path.basename` on the resolved path in order to get the plugin name (for logging purposes).
    
    * log: skip "Loading plugins" message if no plugins were found

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ const registerPlugin = require('./lib/registerPlugin');
 
 // Plugins depended on by the `meta` package.
 const corePlugins = new Map();
-Object.keys(meta.dependencies).forEach(
-  name => /^meta-/.test(name) && corePlugins.set(name, require.resolve(name))
-);
+Object.keys(meta.dependencies).forEach(name => {
+  if (/^meta-/.test(name)) {
+    const packagePath = require.resolve(path.join(name, 'package.json'));
+    corePlugins.set(name, path.dirname(packagePath));
+  }
+});
 
 exports.version = meta.version;
 
@@ -24,8 +27,10 @@ exports.run = (cwd, argv) => {
 
   // Load user plugins.
   const userPlugins = findPlugins(cwd);
-  debug(`\nLoading plugins:`);
-  userPlugins.forEach(pluginPath => registerPlugin(program, pluginPath));
+  if (userPlugins.size) {
+    debug(`\nLoading plugins:`);
+    userPlugins.forEach(pluginPath => registerPlugin(program, pluginPath));
+  }
 
   // Load core plugins after, so users can override them.
   debug(`\nLoading core plugins:`);
