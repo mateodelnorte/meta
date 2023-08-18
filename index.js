1f575d27428fb122e44bed3507bf4ba79372fb17
Your Name <you@example.com>
2023-08-18 16:20:39 +0300

    Added execution summary

--- index.js
+++ index.js
@@ -7,6 +7,12 @@ const meta = require('./package.json');
 const findPlugins = require('./lib/findPlugins');
 const registerPlugin = require('./lib/registerPlugin');
 
+const summary = {
+  success: 0,
+  errors: 0,
+  skipped: 0
+};
+
 // Plugins depended on by the `meta` package.
 const corePlugins = new Map();
 Object.keys(meta.dependencies).forEach((name) => {
@@ -29,14 +35,14 @@ exports.run = (cwd, argv) => {
   const userPlugins = findPlugins(cwd);
   if (userPlugins.size) {
     debug(`\nLoading plugins:`);
-    userPlugins.forEach((pluginPath) => registerPlugin(program, pluginPath));
+    userPlugins.forEach((pluginPath) => registerPlugin(program, pluginPath, summary));
   }
 
   // Load core plugins after, so users can override them.
   debug(`\nLoading core plugins:`);
   corePlugins.forEach((pluginPath, name) => {
     if (userPlugins.has(name)) return debug(`  ${green('+')} ${name} ${gray('(skip)')}`); // prettier-ignore
-    registerPlugin(program, pluginPath);
+    registerPlugin(program, pluginPath, summary);
   });
 
   if (fs.existsSync('.meta')) {
@@ -45,4 +51,12 @@ exports.run = (cwd, argv) => {
   }
 
   program.parse(argv);
+
+  // After processing all plugins:
+  console.log(`\nExecution Summary:`);
+  console.log(`${summary.success}/${userPlugins.size + corePlugins.size} projects finished successfully`);
+  console.log(`${summary.errors}/${userPlugins.size + corePlugins.size} projects had errors`);
+  if (summary.skipped) {
+    console.log(`${summary.skipped}/${userPlugins.size + corePlugins.size} projects were skipped`);
+  }
 };
